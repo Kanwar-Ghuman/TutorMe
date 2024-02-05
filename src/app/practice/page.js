221a427801203c210d0604e483e375ec19cff9a8
Shamit Surana <shamit.surana@gmail.com>
2024-02-05 14:59:53 -0600

    day 1: work

---
+++ src/app/practice/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Practice = () => {
+    // Your base page content goes here
+    return (
+        <>
+            <div className="">Hello World!</div>
+        </>
+    );
+};
+
+export default Practice;
--- src/app/request/create/page.js
+++ src/app/request/create/page.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Input } from "@/components/ui/input"
 
 const CreateRequest = () => {
     // Your base page content goes here
     return (
         <>
-
+            <div class="flex items-center justify-center m-4">
+                <div className="w-1/2">
+                    <h1 className="text-2xl mb-10">Request A Tutor</h1>
+                    <Input className="mb-7" type="name" placeholder="Student Name" />
+                    <Input className="mb-7" type="name" placeholder="Teacher Name" />
+                    
+                </div>
+            </div>
         </>
     );
 };
