c6f7eb252f5be180207577a190fa3e09198b2b88
Shamit Surana <shamit.surana@gmail.com>
2024-02-05 14:59:53 -0600

    day 1: work

--- src/app/request/accept/page.js
+++ src/app/request/accept/page.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Skeleton } from "@/components/ui/skeleton"
 
 const AcceptRequest = () => {
     // Your base page content goes here
     return (
         <>
-
+        <h1 className="text-5xl"><b>hello</b></h1>
         </>
     );
 };
