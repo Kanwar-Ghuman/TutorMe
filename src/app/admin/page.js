243d08885b76c03736cbdbb975cc57edbc33a918
Shamit Surana <shamit.surana@gmail.com>
2024-02-12 15:05:45 -0600

    Starter

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,22 +1,17 @@
 import React from 'react';
+import { TutorCard } from '@/components/tutorme/home/admin/tutorCard';
+import { SideBar } from '@/components/tutorme/home/admin/sideBar';
 
 const AcceptRequest = () => {
     // Your base page content goes here
     return (
         <>
         <div>
-            <button 
-                // CN={c} 
-                // style: {background-color: red}    
-                onClick={console.log("hi")}
-            >press me</button>
+            <SideBar />
+            <TutorCard />
         </div>
         </>
     );
 };
-
-// function print(){
-//     console.log("hi");
-// }
 
 export default AcceptRequest;
