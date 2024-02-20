e0d9c54c7c132e16274d923a74fa2675996fe37a
Anant-raj2 <heyanantraj@gmail.com>
2024-02-20 12:04:16 -0600

    school commit

--- src/components/tutorme/auth/login.js
+++ src/components/tutorme/auth/login.js
@@ -2,18 +2,19 @@
 
 import React from 'react';
 import { signIn } from "next-auth/react";
+import Card from "@/components/ui/card";
 
 export function LoginPage({ providers }) {
   return (
     <div>
       {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-                Sign in with {provider.name}
-            </button>
-            </div>
-        ))}
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+      <Card />
     </div>
   );
 }
-
