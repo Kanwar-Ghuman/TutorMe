7fefb28071b44185e948f6c736f5858786887ef6
Anant-raj2 <heyanantraj@gmail.com>
2024-03-04 15:45:28 -0600

    prisma

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,11 @@
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
