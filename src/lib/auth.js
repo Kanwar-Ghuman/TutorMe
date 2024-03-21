88df92313ededb09f27086a86ba2543e0f8f76c1
Anant-raj2 <heyanantraj@gmail.com>
2024-03-21 14:08:00 -0500

    Bug fixing google auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,10 +8,54 @@ export const authOptions = {
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_SECRET,
+        // }),
         GoogleProvider({
+            name: 'Sign in',
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-        }),
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'email',
+                    placeholder: 'hello@example.com'
+                },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    return null
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user) {
+                    return null
+                }
+
+                const isPasswordValid = await compare(
+                    credentials.password,
+                    user.password
+                )
+
+                if (!isPasswordValid) {
+                    return null
+                }
+
+                return {
+                    id: user.id + '',
+                    email: user.email,
+                    name: user.name,
+                    randomKey: 'Hey cool'
+                }
+            }
+        })
     ],
     pages: {
         signIn: '/auth/login/',
