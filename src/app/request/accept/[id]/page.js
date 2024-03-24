2bbe3d7bd9827c9a60424757186f02e0c333ef8d
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-03-24 13:14:27 -0500

    Fully responsive accept pagee

--- src/app/request/accept/[id]/page.js
+++ src/app/request/accept/[id]/page.js
@@ -4,30 +4,32 @@ import { Button } from "@/components/ui/button";
 const AcceptRequest = () => {
   return (
     <div className="flex flex-col justify-start min-h-screen px-4 sm:px-10 md:px-20 lg:px-32 xl:px-80 pb-[5rem] sm:pb-[10rem] md:pb-[15rem] lg:pb-[20rem] xl:pb-[30rem]">
-      <div className="pt-4 sm:pt-6 md:pt-7 lg:pt-10 xl:pt-[8rem]">
-        <h1 className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold">
+      {/* Increased top padding for the first div to move the text box down a bit */}
+      <div className="pt-10 sm:pt-12 md:pt-14 lg:pt-16 xl:pt-20">
+        {/* Adjusted font size to be larger on smaller screens */}
+        <h1 className="text-md sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">
           <span className="text-[#0E9F6E]">Success!</span> You are now pending
           approval from Mr. Decker.
         </h1>
       </div>
       <div className="mt-4 sm:mt-[4rem]">
-        <p className="text-sm sm:text-md md:text-lg">
+        <p className="text-md sm:text-lg md:text-xl">
           Here are the details of the student you are tutoring:
         </p>
-        <p className="text-sm sm:text-md md:text-lg">
+        <p className="text-md sm:text-lg md:text-xl">
           <span className="font-bold">Name:</span> John Doe
           (john.doe@franklinsabers.org)
         </p>
-        <p className="text-sm sm:setext-md md:text-lg">
+        <p className="text-md sm:text-lg md:text-xl">
           <span className="font-bold">Grade:</span> 10
         </p>
-        <p className="text-sm sm:text-md md:text-lg">
+        <p className="text-md sm:text-lg md:text-xl">
           <span className="font-bold">Subject:</span> Pre-AP Chemistry
         </p>
       </div>
       <div className="mt-12 sm:mt-14 md:mt-20">
-        <p className="text-md sm:text-lg md:text-2xl font-bold">Next Steps?</p>
-        <div className="text-sm sm:text-md md:text-lg">
+        <p className="text-lg sm:text-xl md:text-2xl font-bold">Next Steps?</p>
+        <div className="text-md sm:text-lg md:text-xl">
           <p>
             <span className="font-bold">1)</span> You will receive an email with
             the details of your tutoring arrangement.
@@ -47,7 +49,7 @@ const AcceptRequest = () => {
           </p>
         </div>
       </div>
-      <p className="mt-10 sm:mt-15 md:mt-[8rem] text-xs">
+      <p className="mt-10 sm:mt-15 md:mt-[7rem] text-sm sm:text-md">
         If you would like to cancel your request within the next 10 minutes, you
         can click the button below:
       </p>
