c4ca35baef3cf031f6c171b6c99a21fb106be67b
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-03-24 13:57:51 -0500

    Fully responsive home page

--- src/components/tutorme/auth/login.js
+++ src/components/tutorme/auth/login.js
@@ -6,26 +6,30 @@ import { Button } from "@/components/ui/button";
 
 export const LoginPage = ({ providers }) => {
   return (
-    <div className="bg-[#f7f7f7] flex flex-col md:flex-row justify-between items-center w-full h-full min-h-screen px-4 sm:px-10 md:px-20">
-      <div className="text-left mb-10 md:mb-0 mt-[9rem] md:-mt-20 ml-4 md:ml-20">
-        <p className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-medium leading-none mb-12 md:mb-[6rem]">
+    <div className="bg-[#f7f7f7] flex flex-col md:flex-row justify-start md:justify-between items-center w-full h-full min-h-screen px-4 sm:px-10 md:px-20">
+      <div className="text-left mb-10 mt-[9rem] sm:mt-[5rem] md:mt-[-2rem] ml-4 md:ml-20">
+        <p className="text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl font-medium leading-none mb-8 md:mb-[6rem]">
           Login
         </p>
-        <p className="text-6xl sm:text-8xl md:text-6xl lg:text-7xl xl:text-8xl font-medium leading-tight mb-12 md:mb-[6rem]">
-          to TutorMe
+        <p className="text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl font-medium leading-tight mb-4 md:mb-0 md:inline-flex  pt-[3rem] sm:pt-[5rem]">
+          <span>to TutorMe</span>
         </p>
-        <p className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-medium mb-8 md:mb-12">
-          for <span className="text-yellow-text">Franklin</span>
-        </p>
+        <div className="md:inline-flex md:space-x-8">
+          <p className="text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl font-medium pt-[4rem] sm:pt-[10rem]">
+            <span>
+              for <span className="text-yellow-text">Franklin</span>
+            </span>
+          </p>
+        </div>
       </div>
-      <div className="w-full md:w-auto flex justify-center md:justify-end md:items-end mt-4 md:mt-0">
+      <div className="w-full mt-4 px-4 sm:px-0">
         {Object.values(providers).map((provider) => (
           <div
             key={provider.name}
-            className="w-full md:w-auto flex justify-center md:justify-end mt-4"
+            className="mt-4 flex justify-center md:justify-end"
           >
             <Button
-              className="w-[300px] h-[50px] "
+              className="w-[300px] h-[50px]"
               onClick={() => signIn(provider.id)}
             >
               <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
