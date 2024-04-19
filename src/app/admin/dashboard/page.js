7a9ff52f3c05fc74a9c5cd6a1b83249d3e9f50a5
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-04-18 22:44:08 -0500

    Melchoic Admin Page

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { TutorCard } from '@/components/tutorme/home/admin/tutorCard';
-import { SideBar } from '@/components/tutorme/home/admin/sideBar';
+import React from "react";
+("use-client");
+
+// import { Inter } from 'next/font/google'
+// import { useState } from "react";
+
+import Navbar from "@/components/tutorme/home/nav/navbar";
+import SideBar from "./sideBar";
+import ScrollBar from "./scrollBar";
 
 const AcceptRequest = () => {
-    // Your base page content goes here
-    return (
-        <>
-        <div>
-            <button 
-                onClick={console.log("hi")}
-            >press me</button>
+  // Your base page content goes here
+  function mouseOver(event) {
+    event.target.style.background = "red";
+  }
+
+  // const myHeight = window.innerHeight * 0.5;
+  return (
+    <div
+      // asChild
+      style={{ height: "100vh" }}
+      className="scroll-hide"
+    >
+      <Navbar />
+      <div className="flex w-100 space-x-4 p-4">
+        <div className="w-[20%] shadow-md rounded-lg">
+          <SideBar />
         </div>
-        </>
-    );
+        <div className="w-[80%]">
+          <ScrollBar />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AcceptRequest;
---
+++ src/components/request/accept/acceptStudentCard.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function acceptStudentCard(props) {
+  return (
+    <Card className="m-4 transition-shadow hover:shadow-md hover:cursor-pointer">
+      <CardContent className="p-4">
+        <div className="flex justify-between">
+          <div className="flex flex-col space-y-3">
+            <div>
+              <span className="font-bold">Student:</span> {props.studentName}
+            </div>
+            <div>
+              <span className="font-bold">Tutor:</span> {props.tutorName}
+            </div>
+
+            <CardDescription>Click to view more information</CardDescription>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <Button variant="success">Accept</Button>
+            <Button variant="destructive">Decline</Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
