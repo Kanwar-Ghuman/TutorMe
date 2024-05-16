3506f2bdf1e2061d30dc2190c95bf800b40d10cb
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-05-15 21:50:13 -0500

    Added in loading skeleton

--- src/app/teacher/pastRequest/page.js
+++ src/app/teacher/pastRequest/page.js
@@ -1,15 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   Button,
+  Skeleton,
 } from "@nextui-org/react";
-
-import { useState, useEffect } from "react";
 
 const PastRequests = () => {
   const [requests, setRequests] = useState([]);
@@ -34,6 +33,32 @@ const PastRequests = () => {
 
     fetchRequests();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center w-full">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Card key={index} className="w-[1500px] mb-4 p-4 space-y-5">
+            <Skeleton className="rounded-lg">
+              <div className="h-24 rounded-lg bg-default-300"></div>
+            </Skeleton>
+            <div className="space-y-3">
+              <Skeleton className="w-3/5 rounded-lg">
+                <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+              </Skeleton>
+              <Skeleton className="w-4/5 rounded-lg">
+                <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+              </Skeleton>
+              <Skeleton className="w-2/5 rounded-lg">
+                <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+              </Skeleton>
+            </div>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center w-full">
       {requests.map((request) => (
