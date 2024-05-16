a6b6f22fd9cfb72168451c2d9a4921407da8c057
Rohan-Peddamallu <87048658+Rohan-Peddamallu@users.noreply.github.com>
2024-05-16 14:56:07 -0500

    navigationdone

---
+++ src/components/tutorme/home/nav/adminNavbar.js
@@ -0,0 +1,34 @@
+"use server";
+import { BaseNavbar } from "./baseNavbar";
+
+export async function AdminNavbar({ user }) {
+  const menuItems = [
+    { label: "Dashboard", link: "/admin/dashboard" },
+    { label: "Create a Request", link: "/teacher/request/create" },
+  ];
+
+  const profile = [
+    {
+      key: "profile",
+      label: (
+        <>
+          <p className="font-semibold">Signed in as</p>
+          <p className="font-semibold">{user.email}</p>
+        </>
+      ),
+      className: "h-14 gap-2",
+      link: "/profile",
+    },
+    { key: "settings", label: "Settings", link: "/settings" },
+    { key: "team_settings", label: "Team Settings", link: "/team-settings" },
+    { key: "analytics", label: "Analytics", link: "/analytics" },
+    { key: "logout", label: "Log Out", color: "danger" },
+  ];
+
+  return (
+    <BaseNavbar
+      menuItems={menuItems}
+      profileItems={[[user.name, user.image], profile]}
+    />
+  );
+}
