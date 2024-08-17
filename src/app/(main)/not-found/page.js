6c9432fef6c2f4581012a5b7ce6b25dedb06460c
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-08-17 12:02:19 -0500

    test

--- src/app/(main)/not-found/page.js
+++ src/app/(main)/not-found/page.js
@@ -18,7 +18,7 @@ const NotFound = () => {
       <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold -mt-20 sm:-mt-24 md:-mt-28 lg:-mt-32  font-inter">
         Sorry, you don&apos;t have
       </h1>
-      <h2 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mt-5  font-inter ">
+      <h2 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mt-4  font-inter ">
         access to this beta
       </h2>
       <p
