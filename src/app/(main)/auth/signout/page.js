ddbcef16092c31f4288108feb756dab81e174e71
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-08-17 12:07:34 -0500

    fix

--- src/app/(main)/auth/signout/page.js
+++ src/app/(main)/auth/signout/page.js
@@ -1,23 +1,22 @@
 import { getProviders } from "next-auth/react";
-import { getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/auth";
 import LoginPage from "@/components/tutorme/auth/login";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import SignOut from "./signout";
 
 export default async function Signout(context) {
-
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   if (!session) {
-    return redirect("/auth/login")
+    return redirect("/auth/login");
   }
 
-  const providers = await getProviders()
+  const providers = await getProviders();
 
   return (
     <>
       <SignOut />
     </>
-  )
+  );
 }
