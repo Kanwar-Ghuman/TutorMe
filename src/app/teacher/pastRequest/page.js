17dcb39f58dee6be715a8f541ab845e7ae080eb8
Ameentheguy <Ameenalmousa0@gmail.com>
2024-08-17 22:47:24 -0500

    worked on past requests

--- src/app/teacher/pastRequest/page.js
+++ src/app/teacher/pastRequest/page.js
@@ -1,6 +1,12 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsGenderMale, BsGenderFemale } from "react-icons/bs";
+import { PiBooks } from "react-icons/pi";
+import { RxDividerVertical } from "react-icons/rx";
+import { IoEllipsisVerticalOutline } from "react-icons/io5";
+
 import {
   Card,
   CardHeader,
@@ -58,7 +64,7 @@ const PastRequests = () => {
     return (
       <div className="flex flex-row flex-wrap w-full py-5">
         {Array.from({ length: 9 }).map((_, index) => (
-          <Card key={index} className="w-[1500px] sm:w-[300px] h-[200px] mb-4 p-4 space-y-5 mx-5">
+          <Card key={index} className="w-[1500px] sm:w-[400px] h-[200px] mb-4 p-4 space-y-5 mx-5">
             <Skeleton className="rounded-lg">
               <div className="h-24 rounded-lg bg-default-300"></div>
             </Skeleton>
@@ -80,23 +86,47 @@ const PastRequests = () => {
   }
 
   return (
-    <div className="flex flex-wrap flex-row w-full p-5"> 
+    <div className="flex flex-wrap flex-row w-full p-5 "> 
       {requests.length === 0 ? (
         <div className="flex flex-col items-center justify-center h-full">
           <p className="text-2xl text-black-500">No Requests Found</p>
         </div>
       ) : (
         requests.map((request) => (
-          <Card key={request.id} className="w-[1500px] mb-4 sm:w-[300px] h-[200px] mx-5">
+          <Card key={request.id} className="w-[1500px] mb-4 sm:w-[300px] h-[210px] mx-4 bg-white shadow-md  hover:shadow-[#FACC14] border border-black transition-transform duration-200 ease-in-out hover:scale-105 overflow-hidden">
             <strong>
               <CardHeader className="text-black-700 text-m items-center justify-center">
                 {request.student}
               </CardHeader>
             </strong>
-            <CardBody className="text-blue-600 gap-4">
-              <p>Email: {request.studentEmail}</p>
-              <p>Subject: {request.subject}</p>
-              <p>Gender Preference: {request.genderPref} </p>
+            <CardBody className="text-black gap-4">
+              <div className="flex items-center gap-1">       
+                <MdOutlineEmail/>       
+                <IoEllipsisVerticalOutline size={20}/>
+                <p>{request.studentEmail}</p>
+                </div>
+              <div className="flex items-center gap-1">
+                <PiBooks />
+                <IoEllipsisVerticalOutline size={20}/>
+                <p>{request.subject}</p>
+              </div>
+              <div className="flex items-center gap-2">
+                {request.genderPref === "F" ? (
+                  <BsGenderFemale />
+                ): request.genderPref === "M" ? (
+                  <BsGenderMale />
+                ): (<>
+                  <BsGenderFemale />
+                  <BsGenderMale />
+                  </>
+                  )}
+                  <IoEllipsisVerticalOutline size={20}/>
+                {request.genderPref === "F" ? (
+                  <p>Female</p>
+                ): (request.genderPref === "M" ? (
+                  <p>Male</p>
+                ): (<p>No Preference</p>))} 
+              </div>
             </CardBody>
           </Card>
         ))
