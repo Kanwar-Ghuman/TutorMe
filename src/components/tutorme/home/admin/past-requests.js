90661490e295e93bb4672674d4a5d2a499dc8269
Rohan-Peddamallu <rohanpeddamallu@gmail.com>
2024-08-23 21:57:36 -0500

    Added filter button

--- src/components/tutorme/home/admin/past-requests.js
+++ src/components/tutorme/home/admin/past-requests.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import { Spinner } from "@nextui-org/react";
 import { Input } from "@/components/ui/input";
 import StudentCard from "./studentCard";
+import { IoFilter } from "react-icons/io5";
+import Select from "react-select";
 
 const PastRequests = () => {
   const [studentArr, setStudentArr] = useState([]);
@@ -79,11 +81,23 @@ const PastRequests = () => {
     <div className="h-full w-full flex flex-col items-center">
       <>
         <div className="flex flex-row m-4 justify-center items-center w-full">
+        <Select
+                      
+                      className="min-w-[15%] h-10 px-4 basic-multi-select"
+                      classNamePrefix="select"
+                      placeholder={
+                        <div className="flex items-center">
+                          <IoFilter className="mr-2" />
+                          <span>Filter</span>
+                        </div>
+                      }
+                      
+                    />
           <Input
             type="text"
             id="inputSearch"
             placeholder="Search"
-            className="w-[80%]"
+            className="w-[60%]"
             onKeyUp={(event) => {
               search(event.target.value);
             }}
@@ -95,7 +109,7 @@ const PastRequests = () => {
               <p className="text-gray-500 text-lg">No results found</p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-8 p-4 mx-auto max-w-6xl">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 sm:gap-x-10 gap-x-20 gap-y-10 p-4 mx-auto max-w-6xl">
               {listStudent.map((student) => (
                 <StudentCard
                   id={student.id}
