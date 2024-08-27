653bb8f328b3310825e98d3b09f3db3e8d2e0809
Ameentheguy <Ameenalmousa0@gmail.com>
2024-08-26 21:28:12 -0500

    added options to admin select

--- src/components/tutorme/home/admin/past-requests.js
+++ src/components/tutorme/home/admin/past-requests.js
@@ -38,6 +38,51 @@ const PastRequests = () => {
   const [success, setSuccess] = useState(false);
   const [tutors, setTutors] = useState([]);
 
+  const subjectsOptions = [
+    {
+      label: "Math",
+      options: [
+        { value: "IM1", label: "IM1" },
+        { value: "IM2", label: "IM2" },
+        { value: "IM3", label: "IM3" },
+        { value: "Precalc", label: "Precalculus" },
+        { value: "Calc AB", label: "AP Calculus AB" },
+        { value: "Calc BC", label: "AP Calculus BC" },
+      ],
+    },
+    {
+      label: "Science",
+      options: [
+        { value: "Physics", label: "Physics" },
+        { value: "Chemistry", label: "Chemistry" },
+        { value: "Biology", label: "Biology" },
+        { value: "AP Physics", label: "AP Physics" },
+        { value: "AP Chemistry", label: "AP Chemistry" },
+        { value: "AP Biology", label: "AP Biology" },
+      ],
+    },
+    {
+      label: "Spanish",
+      options: [
+        { value: "Spanish 1", label: "Spanish 1" },
+        { value: "Spanish 2", label: "Spanish 2" },
+        { value: "Spanish 3", label: "Spanish 3" },
+        { value: "Spanish 4", label: "Spanish 4" },
+        { value: "Spanish 5", label: "Spanish 5" },
+      ],
+    },
+    {
+      label: "German",
+      options: [
+        { value: "German 1", label: "German 1" },
+        { value: "German 2", label: "German 2" },
+        { value: "German 3", label: "German 3" },
+        { value: "German 4", label: "German 4" },
+        { value: "German 5", label: "German 5" },
+      ],
+    },
+  ];
+
   useEffect(() => {
     const fetchRequests = async () => {
       try {
@@ -130,10 +175,11 @@ const PastRequests = () => {
           <Select
             className="w-[20%] h-10 px-4 basic-multi-select"
             classNamePrefix="select"
+            options={subjectsOptions}
             placeholder={
               <div className="flex items-center">
                 <IoFilter className="mr-2" />
-                <span>Filter By Subject</span>
+                <span>Filter</span>
               </div>
             }
           />
