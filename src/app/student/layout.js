d0cbd79aee1df887a27dc46f9c808b1128af91c6
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-08-27 23:28:19 -0500

    changes

--- src/app/student/layout.js
+++ src/app/student/layout.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { PlainNavbar } from '@/components/tutorme/home/nav/plainNavbar';
-import { getFrontendPermission } from '@/lib/auth/roles';
+import React from "react";
+import { PlainNavbar } from "@/components/tutorme/home/nav/plainNavbar";
+import { getFrontendPermission } from "@/lib/auth/roles";
 
 async function Layout({ children }) {
-    // const response = await getFrontendPermission("student");
-    // if (!response.isValid) return response.error;
-    // const user = response.user;
+  const response = await getFrontendPermission("student");
+  if (!response.isValid) return response.error;
+  const user = response.user;
 
-    return (
-        <div>
-            {/* Student Navbar (logged in with magic link to email) */}
-            {children}
-        </div>
-    );
-};
+  return (
+    <div>
+      <PlainNavbar />
+      {children}
+    </div>
+  );
+}
 
 export default Layout;
