1fea36609893c4363b2806eaf039e5971ac19760
Ameentheguy <Ameenalmousa0@gmail.com>
2024-08-30 21:00:53 -0500

    fixed login page redirect

--- src/lib/auth/roles.js
+++ src/lib/auth/roles.js
@@ -2,7 +2,7 @@ import { isLoggedIn } from "./validate";
 import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 
-export async function getBackendPermission(app="") {
+export async function getBackendPermission(app = "") {
   const response = await isLoggedIn();
   if (!response.isValid)
     return {
@@ -22,70 +22,70 @@ export async function getBackendPermission(app="") {
     return ["teacher", "admin"].includes(user.role)
       ? { isValid: true, user: user }
       : {
-        isValid: false,
-        error: NextResponse.json({ error: "Unauthorized" }, { status: 401 }),
-      };
+          isValid: false,
+          error: NextResponse.json({ error: "Unauthorized" }, { status: 401 }),
+        };
   } else if (app === "student") {
     return ["student", "admin"].includes(user.role)
       ? { isValid: true, user: user }
       : {
-        isValid: false,
-        error: NextResponse.json({ error: "Unauthorized" }, { status: 401 }),
-      };
+          isValid: false,
+          error: NextResponse.json({ error: "Unauthorized" }, { status: 401 }),
+        };
   } else {
     return {
-        isValid: true,
-        user: user,
+      isValid: true,
+      user: user,
     };
   }
 }
 
-export async function getFrontendPermission(app="") {
-    const response = await isLoggedIn();
-    if (!response.isValid)
-      return {
-        isValid: false,
-        error: redirect("/auth/login")
-      };
-    const user = response.user;
-  
-    if (app === "admin") {
-      return user.role === "admin"
-        ? { isValid: true, user: user }
-        : {
-            isValid: false,
-            error: redirect("/auth/login")
-          };
-    } else if (app === "teacher") {
-      return ["teacher", "admin"].includes(user.role)
-        ? { isValid: true, user: user }
-        : {
-            isValid: false,
-            error: redirect("/auth/login")
-          };
-    } else if (app === "student") {
-      return ["student", "admin"].includes(user.role)
-        ? { isValid: true, user: user }
-        : {
-            isValid: false,
-            error: redirect("/auth/login")
-          };
-    } else {
-      return {
-        isValid: true,
-        user: user
-      };
-    }
+export async function getFrontendPermission(app = "") {
+  const response = await isLoggedIn();
+  if (!response.isValid)
+    return {
+      isValid: false,
+      error: redirect("/auth/login"),
+    };
+  const user = response.user;
+
+  if (app === "admin") {
+    return user.role === "admin"
+      ? { isValid: true, user: user }
+      : {
+          isValid: false,
+          error: redirect("/auth/login"),
+        };
+  } else if (app === "teacher") {
+    return ["teacher", "admin"].includes(user.role)
+      ? { isValid: true, user: user }
+      : {
+          isValid: false,
+          error: redirect("/auth/login"),
+        };
+  } else if (app === "student") {
+    return ["student", "admin"].includes(user.role)
+      ? { isValid: true, user: user }
+      : {
+          isValid: false,
+          error: redirect("/auth/login"),
+        };
+  } else {
+    return {
+      isValid: true,
+      user: user,
+    };
   }
+}
 
 export function getApp(user) {
-    console.log(user)
+  console.log(user);
   if (user.role === "admin") {
-    return "/admin/dashboard";
+    return "/admin/tutor-requests";
   } else if (user.role === "teacher") {
     return "/teacher/dashboard";
   } else if (user.role === "student") {
-    return "/student/dashboard";
+    return "/student/tutor-requests";
   } else {
     return "/unauthorized";
   }
