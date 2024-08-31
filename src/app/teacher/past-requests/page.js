46df22428978df3afc1b48c4f7f1e340f038b3b8
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-08-30 22:21:59 -0500

    fixed loading

--- src/app/teacher/past-requests/page.js
+++ src/app/teacher/past-requests/page.js
@@ -40,63 +40,65 @@ const TeacherPastRequests = () => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex-1 flex items-center justify-center min-h-screen">
         <Spinner size="lg" />
       </div>
     );
   }
   return (
-    <>
-      <div className="flex flex-wrap flex-col items-start w-full p-4">
-        <div className="flex flex-row m-4 justify-center items-center w-full space-x-4 z-50">
-          <Select
-            className="min-w-[15%] h-10 px-4 basic-multi-select"
-            classNamePrefix="select"
-            options={subjectsOptions}
-            placeholder={
-              <div className="flex items-center">
-                <IoFilter className="mr-2" />
-                <span>Filter</span>
-              </div>
-            }
-            styles={customStyles}
-            formatOptionLabel={formatOptionLabel}
-            isClearable={true}
-          />
-
-          <div className="w-2/3">
-            <Input
-              type="text"
-              id="inputSearch"
-              placeholder="Search"
-              className=" w-[80%]"
-              onKeyUp={(event) => {
-                search(event.target.value);
-              }}
-              startContent={
-                <IoSearchOutline className="text-gray-400 pointer-events-none flex-shrink-0" />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-1">
+        <div className="flex flex-wrap flex-col items-start w-full p-4">
+          <div className="flex flex-row m-4 justify-center items-center w-full space-x-4 z-50">
+            <Select
+              className="min-w-[15%] h-10 px-4 basic-multi-select"
+              classNamePrefix="select"
+              options={subjectsOptions}
+              placeholder={
+                <div className="flex items-center">
+                  <IoFilter className="mr-2" />
+                  <span>Filter</span>
+                </div>
               }
+              styles={customStyles}
+              formatOptionLabel={formatOptionLabel}
+              isClearable={true}
             />
-          </div>
-        </div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 mt-8 ">
-            {requests.map((request) => (
-              <TutorCard
-                key={request.id}
-                name={request.student}
-                email={request.studentEmail}
-                subject={request.subject}
+
+            <div className="w-2/3">
+              <Input
+                type="text"
+                id="inputSearch"
+                placeholder="Search"
+                className=" w-[80%]"
+                onKeyUp={(event) => {
+                  search(event.target.value);
+                }}
+                startContent={
+                  <IoSearchOutline className="text-gray-400 pointer-events-none flex-shrink-0" />
+                }
               />
-            ))}
+            </div>
           </div>
-        )}
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error}</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 mt-8 ">
+              {requests.map((request) => (
+                <TutorCard
+                  key={request.id}
+                  name={request.student}
+                  email={request.studentEmail}
+                  subject={request.subject}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
