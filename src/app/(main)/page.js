8d7ad607df910141bf56425ec5b0485bb64b6385
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-10-18 23:06:00 -0500

    Fixed intial load times

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,14 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import LoginPage from "@/components/tutorme/auth/login";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/auth/login");
-  }, [router]);
-
-  return null;
+  return <LoginPage />;
 }
