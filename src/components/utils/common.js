22356b59340d1e2e66240c054570f736b2747be1
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2025-08-20 17:10:37 -0500

    fix

--- src/components/utils/common.js
+++ src/components/utils/common.js
@@ -361,7 +361,7 @@ export const formSubjectsOptions = [
   },
   {
     label: "English",
-    options: [
+    group: [
       {
         value: "AP Language",
         label: "AP Language",
@@ -372,10 +372,10 @@ export const formSubjectsOptions = [
       },
       {
         value: "Pre-AP English 1",
-        label: "Pre-AP English 2",
+        label: "Pre-AP English 1",
       },
       {
-        value: "Pre-AP English 1",
+        value: "Pre-AP English 2",
         label: "Pre-AP English 2",
       },
       {
